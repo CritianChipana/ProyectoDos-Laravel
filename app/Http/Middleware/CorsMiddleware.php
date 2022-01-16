5d5fd4deb88683dcfd3d4b8eddee1bf503b511e7
Local Developer Marco <mterrones.ra@gmail.com>
2022-01-15 21:43:12 -0500

    dasd

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -23,8 +23,7 @@ class CorsMiddleware
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Authorization',
-            'Access-Control-Allow-Headers'     => 'Content-Type'
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization'
             
         ];
 
